95f3b91215e2884cb527cbd6ff9065ec854b5038
Tolulope <tolusnehhil@gmail.com>
2026-02-12 11:56:00 +0100

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-use client";
+"use client";
 import React, { useState } from "react";
 import { ThemeProvider, createTheme, useTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
